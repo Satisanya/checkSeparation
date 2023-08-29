ad0ad48c641545b720bbc07cd46d9ca65eaffce0
alexander <novoseltsev7g21@gmail.com>
2023-08-29 21:50:12 +0300

    29.08-adding-first-component
    I added first main component to test how it works (changes in fork)

--- src/App.vue
+++ src/App.vue
@@ -17,19 +17,25 @@
         <v-divider></v-divider>
 
         <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+          <v-list-item prepend-icon="mdi-plus" title="Calculate new check" value="newCheck"></v-list-item>
+          <v-list-item prepend-icon="mdi-information" title="How to use" value="howTo"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Credits" value="credits"></v-list-item>
         </v-list>
       </v-navigation-drawer>
 
-      <v-main style="height: 250px"></v-main>
+      <v-main style="height: max-content">
+        <Main></Main>
+      </v-main>
     </v-layout>
   </v-card>
 </template>
 
 <script>
+import Main from "@/components/main.vue";
 export default {
+  components:{
+    Main
+  },
   data(){
     return{
       users: [
